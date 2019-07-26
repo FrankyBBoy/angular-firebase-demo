98239aaf2d0d4590209411f4f97c6adfc89ccbc7
Brunet, Francis <Francis.Brunet@lacapitale.com>
2019-07-26 16:41:59 -0400

    Basics components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrdersService } from "./shared/orders.service";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [OrdersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from "../shared/orders.service";
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+
+  coffees = ["Americano", "Flat White", "Cappuccino", "Latte", "Espresso", "Machiato", "Mocha", "Hot Chocolate", "Tea"];
+  coffeeOrder = [];
+
+  constructor(private ordersService:OrdersService) { }
+
+  ngOnInit() {
+  }
+
+  addCoffee = coffee => this.coffeeOrder.push(coffee);
+
+  removeCoffee = coffee => {
+      let index = this.coffeeOrder.indexOf(coffee);
+      if (index > -1) this.coffeeOrder.splice(index, 1);
+  };
+
+  onSubmit() {
+    
+  }
+}
