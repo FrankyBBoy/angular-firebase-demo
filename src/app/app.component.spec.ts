aecd61e068c8e66ff3c53f5c0179eb4234cb5e18
Brunet, Francis <Francis.Brunet@lacapitale.com>
2019-07-27 08:52:37 -0400

    Complete DEMO

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-firebase-demo'`, () => {
+  it(`should have as title 'AngularFirebaseCRUD'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-firebase-demo');
+    expect(app.title).toEqual('AngularFirebaseCRUD');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-firebase-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularFirebaseCRUD!');
   });
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyBqPnLMHQRITjpuwjMzTEnpR34ndaft5a0",
+    authDomain: "angular-firebase-demo-2f622.firebaseapp.com",
+    databaseURL: "https://angular-firebase-demo-2f622.firebaseio.com",
+    projectId: "angular-firebase-demo-2f622",
+    storageBucket: "angular-firebase-demo-2f622.appspot.com",
+    messagingSenderId: "386900127745",
+    appId: "1:386900127745:web:3920ec5abbcd59c9"
+  }
 };
 
 /*
