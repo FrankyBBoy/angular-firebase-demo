8f7f4f0172b729a8ff21c7fcbbf8511fd63542a0
Brunet, Francis <Francis.Brunet@lacapitale.com>
2019-07-27 09:13:48 -0400

    Settings firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyBqPnLMHQRITjpuwjMzTEnpR34ndaft5a0",
+    authDomain: "angular-firebase-demo-2f622.firebaseapp.com",
+    databaseURL: "https://angular-firebase-demo-2f622.firebaseio.com",
+    projectId: "angular-firebase-demo-2f622",
+    storageBucket: "angular-firebase-demo-2f622.appspot.com",
+    messagingSenderId: "386900127745",
+    appId: "1:386900127745:web:3920ec5abbcd59c9"
+  }
 };
